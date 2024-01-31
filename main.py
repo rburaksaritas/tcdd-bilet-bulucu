201f5bc95ccf85a056cd8dd14778f94aa10e8206
burak <rburaksaritas@gmail.com>
2024-01-31 04:20:30 +0300

    Enhance terminal logs

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 sys.dont_write_bytecode = True
 
-from src.checker import fetch_and_filter_journeys
+from src.functions import fetch_and_filter_journeys
 import config
 import time
 
 def main():
     while True:
+        print(f"### Started checking.")
         fetch_and_filter_journeys()
+        print(f"### Completed checking. \n### Will restart in {config.sleep_time} seconds.")
         time.sleep(config.sleep_time)
 
 
